226cfe6b58dbef4948c57b4abb99d27fb394c014
kentaro ishida <ken.babuo@gmail.com>
2023-11-12 16:27:19 +0900

    feat: Write Server

---
+++ backend/applications/write-api-server/src/main.rs
@@ -0,0 +1,24 @@
+use axum::Router;
+use dotenv::dotenv;
+use lambda_http::{run, Body, Error};
+use sqlx::{MySql, MySqlPool, Pool};
+use std::env;
+use utoipa::OpenApi;
+use utoipa_swagger_ui::SwaggerUi;
+use write_api_server::ApiDoc;
+
+use infra::controllers::write_server::create_router;
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    dotenv().ok();
+
+    let database_url: String = env::var("DATABASE_URL").unwrap();
+
+    let pool: Pool<MySql> = MySqlPool::connect(&database_url).await.unwrap();
+
+    let app: Router<(), Body> = create_router(pool)
+        .merge(SwaggerUi::new("/swagger-ui").url("/api-docs/openapi.json", ApiDoc::openapi()));
+
+    run(app).await
+}
