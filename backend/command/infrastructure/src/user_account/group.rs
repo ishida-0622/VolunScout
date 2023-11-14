8b22d197edfeac8394efe64a75af48b68c6ae6f0
kentaro ishida <ken.babuo@gmail.com>
2023-11-14 09:21:48 +0900

    clean: コメントアウトの削除

--- backend/command/infrastructure/src/user_account/group.rs
+++ backend/command/infrastructure/src/user_account/group.rs
@@ -77,36 +77,4 @@ impl GroupUserRepository for GroupAccountImpl {
         .await?;
         Ok(())
     }
-
-    // async fn find_by_id(&self, gid: &UserId) -> Result<GroupAccount> {
-    //     let group: GroupAccount = sqlx::query_as!(
-    //         GroupAccount,
-    //         "SELECT * FROM group_account WHERE gid = ?",
-    //         gid.to_string()
-    //     )
-    //     .fetch_one(&self.pool)
-    //     .await?;
-    //     Ok(group)
-    // }
-
-    // async fn find_by_ids(&self, gids: &[UserId]) -> Result<Vec<GroupAccount>> {
-    //     let groups = sqlx::query_as!(
-    //         GroupAccount,
-    //         "SELECT * FROM group_account WHERE gid IN (?)",
-    //         gids.iter()
-    //             .map(|gid| gid.to_string())
-    //             .collect::<Vec<String>>()
-    //             .join(",")
-    //     )
-    //     .fetch_all(&self.pool)
-    //     .await?;
-    //     Ok(groups)
-    // }
-
-    // async fn find_all(&self) -> Result<Vec<GroupAccount>> {
-    //     let groups = sqlx::query_as!(GroupAccount, "SELECT * FROM group_account")
-    //         .fetch_all(&self.pool)
-    //         .await?;
-    //     Ok(groups)
-    // }
 }
