428dd04e4d576c4a13e0b7b2e955195c54f09916
kentaro ishida <ken.babuo@gmail.com>
2023-11-14 11:23:56 +0900

    feat: グループアカウント情報取得のクエリを実装

--- backend/query/infrastructure/src/resolvers.rs
+++ backend/query/infrastructure/src/resolvers.rs
@@ -1,4 +1,4 @@
-use std::sync::Arc;
+use std::{str::FromStr, sync::Arc};
 
 use anyhow::Result;
 use async_graphql::{
@@ -8,6 +8,7 @@ use async_graphql::{
 use redis::Client;
 use sqlx::MySqlPool;
 
+use domain::model::user_account::user_id::UserId;
 use query_repository::user_account::group::{GroupAccount, GroupUserRepository};
 
 use crate::user_account::group::GroupAccountImpl;
@@ -27,16 +28,57 @@ pub struct QueryRoot;
 
 #[Object]
 impl QueryRoot {
+    /// 指定されたgidのアカウント情報を取得する
+    ///
+    /// ## 引数
+    /// - `gid` - gid
+    ///
+    /// ## 返り値
+    /// - `GroupAccount` - グループアカウント情報
     async fn get_group_account<'ctx>(
         &self,
         ctx: &Context<'ctx>,
         gid: String,
     ) -> Result<GroupAccount> {
-        let ctx = ctx.data::<ServiceContext>().unwrap();
-        let gid = domain::model::user_account::user_id::UserId::new(&gid).unwrap();
-        let group_account = ctx.group_account_dao.find_by_id(&gid).await?;
+        let ctx: &ServiceContext = ctx.data::<ServiceContext>().unwrap();
+        let gid: UserId = UserId::new(&gid).unwrap();
+        let group_account: GroupAccount = ctx.group_account_dao.find_by_id(&gid).await?;
 
         Ok(group_account)
+    }
+
+    /// 複数指定されたgidのアカウント情報を取得する
+    ///
+    /// ## 引数
+    /// - `gids` - gidの配列
+    ///
+    /// ## 返り値
+    /// - `Vec<GroupAccount>` - グループアカウント情報の配列
+    async fn get_group_accounts<'ctx>(
+        &self,
+        ctx: &Context<'ctx>,
+        gids: Vec<String>,
+    ) -> Result<Vec<GroupAccount>> {
+        let ctx: &ServiceContext = ctx.data::<ServiceContext>().unwrap();
+        let gids: Vec<UserId> = gids
+            .iter()
+            .map(|gid| UserId::from_str(gid).unwrap())
+            .collect();
+
+        let group_accounts: Vec<GroupAccount> = ctx.group_account_dao.find_by_ids(&gids).await?;
+
+        Ok(group_accounts)
+    }
+
+    /// 全てのグループアカウント情報を取得する
+    ///
+    /// ## 返り値
+    /// - `Vec<GroupAccount>` - グループアカウント情報の配列
+    async fn get_all_group_accounts<'ctx>(&self, ctx: &Context<'ctx>) -> Result<Vec<GroupAccount>> {
+        let ctx: &ServiceContext = ctx.data::<ServiceContext>().unwrap();
+        let group_accounts: Vec<GroupAccount> = ctx.group_account_dao.find_all().await?;
+
+        Ok(group_accounts)
     }
 }
 
