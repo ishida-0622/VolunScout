d6a5a5174efee80a28ae2a448f5fab03f9c8bfbd
kentaro ishida <ken.babuo@gmail.com>
2023-11-24 16:00:40 +0900

    feat: ヘッダーを追加

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,9 +1,9 @@
 import { Inter } from "next/font/google";
-import "./globals.css";
 
 import type { Metadata } from "next";
 
-import { WithApollo } from "@/components/WithApollo";
+import { Header } from "@/components/layouts/Header";
+import { WithApollo } from "@/components/layouts/WithApollo";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,9 +18,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="ja">
       <body className={inter.className}>
-        <WithApollo>{children}</WithApollo>
+        <WithApollo>
+          <Header />
+          {children}
+        </WithApollo>
       </body>
     </html>
   );
