cea6f7f0f92b88b89f0a4b2c762e605997d9dbca
MasKatx <tky2102137@stu.o-hara.ac.jp>
2023-11-27 16:42:23 +0900

    fix: sql query about volunteer

--- backend/command/infrastructure/src/activities/volunteer.rs
+++ backend/command/infrastructure/src/activities/volunteer.rs
@@ -6,7 +6,8 @@ use sqlx::MySqlPool;
 use command_repository::activities::volunteer::VolunteerRepository;
 use domain::model::{
     volunteer::VolunteerId,
-    user_account::user_id::UserId
+    user_account::user_id::UserId,
+    terms::Terms
 };
 
 pub struct VolunteerImpl {
@@ -37,7 +38,7 @@ impl VolunteerRepository for VolunteerImpl {
         terms: Terms
     ) -> Result<()> {
         sqlx::query!(
-            "INSERT INTO volunteer (vid, gid, title, message, overview, recruited_num, place, start_at, finish_at, deadline_on, as_group, registerd_at, updated_at) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+            "INSERT INTO volunteer (vid, gid, title, message, overview, recruited_num, place, start_at, finish_at, deadline_on, as_group, registered_at, updated_at) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
             vid.to_string(),
             gid.to_string(),
             title,
@@ -49,7 +50,6 @@ impl VolunteerRepository for VolunteerImpl {
             finish_at,
             deadline_on,
             as_group,
-            false,
             Utc::now(),
             Utc::now()
         )
@@ -73,7 +73,7 @@ impl VolunteerRepository for VolunteerImpl {
         terms: Terms
     ) -> Result<()> {
         sqlx::query!(
-            "UPDATE voluneter SET title = ?, message = ?, overview = ?, recruited_num = ?, place = ?, start_at = ?, finish_at = ?, deadline_on = ?, as_group = ?, updated_at = ?, WHERE vid = ?",
+            "UPDATE volunteer SET title = ?, message = ?, overview = ?, recruited_num = ?, place = ?, start_at = ?, finish_at = ?, deadline_on = ?, as_group = ?, updated_at = ? WHERE vid = ?",
             title,
             message,
             overview,
