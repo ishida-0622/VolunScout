e3ab744a620fc363cb64938dec116769224b6546
kentaro ishida <ken.babuo@gmail.com>
2023-11-27 09:31:26 +0900

    feat: react-hook-form で使用する トグルボタン

---
+++ frontend/src/components/ui-parts/ToggleSwitchControl/index.tsx
@@ -0,0 +1,29 @@
+import styles from "./index.module.css";
+
+import type { FieldPath, FieldValues, UseFormRegister } from "react-hook-form";
+
+import { joinClassnames } from "@/components/@joinClassnames";
+
+type Props<T extends FieldValues> = {
+  name: FieldPath<T>;
+  value: string;
+  register: UseFormRegister<T>;
+  label: string;
+  className?: string;
+};
+
+export const ToggleSwitchControl = <T extends FieldValues>({
+  name,
+  value,
+  register,
+  label,
+  className,
+}: Props<T>) => {
+  return (
+    <label className={joinClassnames(styles.base, className)}>
+      <input type="checkbox" value={value} {...register(name)} />
+      <span />
+      {label}
+    </label>
+  );
+};
