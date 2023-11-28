50e4e2b1ba4d6b2fb5c02b0c5ebab75f5d070327
MasKatx <tky2102137@stu.o-hara.ac.jp>
2023-11-28 11:34:50 +0900

    feat: Regionと同等の実装に変更・DisplayとFromStrの実装

--- backend/domain/src/model/theme.rs
+++ backend/domain/src/model/theme.rs
@@ -1,22 +1,49 @@
+use anyhow::Result;
 use serde::{Deserialize, Serialize};
+use thiserror::Error;
 
-use crate::consts::themes::THEMES;
+use crate::consts::themes::{THEMES, ThemeMap};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Theme {
-    pub theme: String,
+    theme: String,
 }
 
-#[derive(Debug)]
-pub struct ThemeNotFoundError;
+#[derive(Error, Debug)]
+pub enum ThemeError {
+    #[error("theme not found")]
+    NotFound,
+}
 
 impl Theme {
-    pub fn new(theme: String) -> Result<Theme, ThemeNotFoundError> {
-        for t in THEMES {
-            if t == &theme {
+    pub fn new(theme: String) -> Result<Theme> {
+        for r in THEMES {
+            if r == &theme {
                 return Ok(Theme { theme });
             }
         }
-        Err(ThemeNotFoundError)
+        Err(ThemeError::NotFound.into())
+    }
+
+    pub fn to_uint(&self) -> u8 {
+        let theme_map: ThemeMap = ThemeMap::new();
+        let index: &usize = theme_map.themes_name_to_index.get(&self.theme).unwrap();
+        *index as u8
+    }
+}
+
+// Displayを実装することで, to_string()で文字列に変換できるようになる
+impl std::fmt::Display for Theme {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", &self.theme)
+    }
+}
+
+// FromStrを実装することで, from_str()で文字列から変換できるようになる
+impl std::str::FromStr for Theme {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Theme::new(s.to_string())
     }
 }
