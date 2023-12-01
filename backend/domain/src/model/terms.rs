f7e99fd43435c12bbd1ed50b99da0d2bfbbcf3fd
MasKatx <tky2102137@stu.o-hara.ac.jp>
2023-12-01 14:53:35 +0900

    feat: issue#15

--- backend/domain/src/model/terms.rs
+++ backend/domain/src/model/terms.rs
@@ -1,12 +1,14 @@
 use serde::{Deserialize, Serialize};
 
-use crate::model::{region::Region, target_status::TargetStatus, theme::Theme};
+use crate::model::{region::Region, target_status::TargetStatus, theme::Theme, condition::Condition};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Terms {
     pub regions: Vec<Region>,
     pub themes: Vec<Theme>,
-    pub transportation_expenses: bool,
+    pub required_themes: Vec<Theme>,
+    pub conditions: Vec<Condition>,
+    pub required_conditions: Vec<Condition>,
     pub reward: Option<String>,
     pub target_status: Vec<TargetStatus>,
 }
@@ -15,14 +17,18 @@ impl Terms {
     pub fn new(
         regions: Vec<Region>,
         themes: Vec<Theme>,
-        transportation_expenses: bool,
+        required_themes: Vec<Theme>,
+        conditions: Vec<Condition>,
+        required_conditions: Vec<Condition>,
         reward: Option<String>,
         target_status: Vec<TargetStatus>,
     ) -> Terms {
         Terms {
             regions,
             themes,
-            transportation_expenses,
+            required_themes,
+            conditions,
+            required_conditions,
             reward,
             target_status,
         }
