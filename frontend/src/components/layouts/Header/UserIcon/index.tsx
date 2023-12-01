cccf1937b2d153e34649ec30b504cac8af4c8268
kentaro ishida <ken.babuo@gmail.com>
2023-12-01 17:16:49 +0900

    feat: Tooltipを追加

--- frontend/src/components/layouts/Header/UserIcon/index.tsx
+++ frontend/src/components/layouts/Header/UserIcon/index.tsx
@@ -1,47 +1,70 @@
 "use client";
 
 import Image from "next/image";
-import { useRouter } from "next/navigation";
+import Link from "next/link";
+import { useState } from "react";
 
 import styles from "./index.module.css";
 
 import type { AccountType } from "@/features/auth/types";
 
-import { URL_PATH_GROUP, URL_PATH_PARTICIPANT } from "@/consts";
+import { URL_PATH, URL_PATH_GROUP, URL_PATH_PARTICIPANT } from "@/consts";
 import { useUser } from "@/features/auth/hooks/useUser";
+import { logout } from "@/features/auth/utils/logout";
 
 type Props = {
   accountType: AccountType;
 };
 
 export const UserIcon = ({ accountType }: Props) => {
-  const router = useRouter();
   const { user, isLoading } = useUser();
 
-  const handleOnClick = () => {
+  const [isTooltipOpen, setIsTooltipOpen] = useState(false);
+  const toggleTooltip = () => setIsTooltipOpen((prev) => !prev);
+
+  const toMyPage = () => {
     if (accountType === "group") {
-      router.push(URL_PATH_GROUP.ACCOUNT);
+      return URL_PATH_GROUP.ACCOUNT;
     } else if (accountType === "participant") {
-      router.push(URL_PATH_PARTICIPANT.ACCOUNT);
+      return URL_PATH_PARTICIPANT.ACCOUNT;
+    } else {
+      throw new Error("accountType is invalid");
     }
   };
 
   if (isLoading) return null;
 
   return (
-    <Image
-      src={user?.photoURL ?? "/icon.svg"}
-      alt="user icon"
-      // TODO: 画像のサイズを調整する
-      width={100}
-      height={100}
-      onClick={handleOnClick}
-      className={styles.base}
-    />
-    // TODO: カーソルを合わせたら
-    // ・マイページ
-    // ・お問い合わせ
-    // ・ログアウト
-    // みたいなのが表示されるようにする
+    <>
+      <div>
+        <Image
+          src={user?.photoURL ?? "/icon.svg"}
+          alt="user icon"
+          // TODO: 画像のサイズを調整する
+          width={100}
+          height={100}
+          onClick={toggleTooltip}
+          className={styles.base}
+        />
+      </div>
+      {isTooltipOpen && (
+        <div className={styles.tooltip}>
+          <ul>
+            <li>
+              <Link href={toMyPage()}>マイページ</Link>
+            </li>
+            <li>
+              <Link href={URL_PATH.CONTACT}>お問い合わせ</Link>
+            </li>
+            <li>
+              <Link href={URL_PATH.DONATE}>運営への寄付</Link>
+            </li>
+            <li>
+              <span onClick={logout}>ログアウト</span>
+            </li>
+          </ul>
+        </div>
+      )}
+    </>
   );
 };
