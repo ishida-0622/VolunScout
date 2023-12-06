6b6f4e982a42b421658ab41dc610a4af46a79ba5
kentaro ishida <ken.babuo@gmail.com>
2023-12-06 13:17:03 +0900

    feat: CSS ファイルの追加

--- frontend/src/components/layouts/Footer/index.tsx
+++ frontend/src/components/layouts/Footer/index.tsx
@@ -1,14 +1,21 @@
 import Image from "next/image";
 import Link from "next/link";
 
+import styles from "./index.module.css";
+
+import { joinClassnames } from "@/components/@joinClassnames";
 import { URL_PATH } from "@/consts";
+
+type Props = {
+  className?: string;
+};
 
 /**
  * フッター
  */
-export const Footer = () => {
+export const Footer = ({ className }: Props) => {
   return (
-    <footer>
+    <footer className={joinClassnames(styles.base, className)}>
       <div>
         <Image src={"/icon.svg"} alt="Icon" width={50} height={50} />
       </div>
