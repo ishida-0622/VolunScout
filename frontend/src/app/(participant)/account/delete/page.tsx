7fd94d224a52e336d5906f0ed75ff8e0b9314259
kentaro ishida <ken.babuo@gmail.com>
2023-12-06 17:56:47 +0900

    feat: アカウント削除ページ（参加者）

---
+++ frontend/src/app/(participant)/account/delete/page.tsx
@@ -0,0 +1,7 @@
+import { AccountDeletePage } from "@/features/auth/AccoountDeletePage/participant";
+
+const AccountDelete = () => {
+  return <AccountDeletePage />;
+};
+
+export default AccountDelete;
