f75f3f60e0e7fb3edf2dc5032db818b4485b23f4
kentaro ishida <ken.babuo@gmail.com>
2024-01-03 22:03:20 +0900

    feat: `template` に書かれていたロジックを `Header` に移動

--- frontend/src/app/template.tsx
+++ frontend/src/app/template.tsx
@@ -1,51 +1,9 @@
-"use client";
-
-import { usePathname } from "next/navigation";
-
-import type { AccountType } from "@/features/auth/types";
-
 import { Header } from "@/components/layouts/Header";
-import {
-  URL_PATH_GROUP,
-  URL_PATH_PARTICIPANT,
-  isGroupPath,
-  isNoHeaderLink,
-} from "@/consts";
-import { useUser } from "@/features/auth/hooks/useUser";
 
 const Template = ({ children }: { children: React.ReactNode }) => {
-  const pathname = usePathname();
-  const { user, isLoading } = useUser();
-
-  const getAccountType = (): AccountType =>
-    isGroupPath(pathname) ? "group" : "participant";
-
-  const isNoLink = () => {
-    if (isLoading) return true;
-    if (user === null) return true;
-    isNoHeaderLink(pathname);
-  };
-
-  const isNoIcon = () => {
-    if (isLoading) return true;
-    if (user === null) return true;
-
-    switch (pathname) {
-      case URL_PATH_PARTICIPANT.SIGN_UP:
-      case URL_PATH_GROUP.SIGN_UP:
-        return true;
-      default:
-        return false;
-    }
-  };
-
   return (
     <>
-      <Header
-        accountType={getAccountType()}
-        isNoLink={isNoLink()}
-        isNoIcon={isNoIcon()}
-      />
+      <Header />
       {children}
     </>
   );
--- frontend/src/components/layouts/Header/index.tsx
+++ frontend/src/components/layouts/Header/index.tsx
@@ -1,5 +1,8 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 import { GroupHeader } from "./GroupHeader";
 import { ParticipantHeader } from "./ParticipantHeader";
@@ -9,54 +12,65 @@ import styles from "./index.module.css";
 import type { AccountType } from "@/features/auth/types";
 
 import { joinClassnames } from "@/components/@joinClassnames";
-import { URL_PATH_GROUP, URL_PATH_PARTICIPANT } from "@/consts";
+import {
+  URL_PATH_GROUP,
+  URL_PATH_PARTICIPANT,
+  isNoHeaderIcon,
+  isNoHeaderLink,
+} from "@/consts";
+import { AuthProvider, useAuthContext } from "@/contexts/AuthContext";
+import { getAccountTypeFromPath } from "@/features/auth/utils/getAccountType";
 
 type Props = {
-  accountType: AccountType;
-  isNoLink?: boolean;
-  isNoIcon?: boolean;
   className?: string;
 };
 
-export const Header = ({
-  accountType,
-  className,
-  isNoLink = false,
-  isNoIcon = false,
-}: Props) => {
+export const Header = ({ className }: Props) => {
+  const pathname = usePathname();
+  const { isLogged } = useAuthContext();
+
+  const accountType: AccountType = getAccountTypeFromPath(pathname);
+
+  // ログインしていない場合は, ヘッダーのリンクを表示しない
+  const isNoLink = !isLogged || isNoHeaderLink(pathname);
+
+  const isNoIcon = isNoHeaderIcon(pathname);
+
   return (
-    <header
-      className={joinClassnames(
-        styles.base,
-        accountType === "participant" ? styles.participant : styles.group,
-        className
-      )}
-    >
-      <div>
-        <Link
-          href={
-            accountType === "group"
-              ? URL_PATH_GROUP.HOME
-              : URL_PATH_PARTICIPANT.HOME
-          }
-        >
-          <Image src={"/icon.svg"} alt="Icon" width={100} height={100} />
-        </Link>
-      </div>
-      <div>
-        <h1>VolunScout</h1>
-      </div>
-      {isNoLink ? null : (
-        <>
-          {accountType === "group" && <GroupHeader />}
-          {accountType === "participant" && <ParticipantHeader />}
-        </>
-      )}
-      {isNoIcon ? null : (
+    <AuthProvider>
+      <header
+        className={joinClassnames(
+          styles.base,
+          accountType === "participant" ? styles.participant : styles.group,
+          className
+        )}
+      >
         <div>
-          <UserIconOrSignInButton accountType={accountType} />
+          <Link
+            href={
+              accountType === "group"
+                ? URL_PATH_GROUP.HOME
+                : URL_PATH_PARTICIPANT.HOME
+            }
+          >
+            <Image src={"/icon.svg"} alt="Icon" width={100} height={100} />
+          </Link>
         </div>
-      )}
-    </header>
+        <div>
+          <h1>VolunScout</h1>
+        </div>
+        {isNoLink ? null : (
+          <>
+            {accountType === "group" && <GroupHeader />}
+            {accountType === "participant" && <ParticipantHeader />}
+          </>
+        )}
+        {isNoIcon ? null : (
+          <div>
+            <UserIconOrSignInButton accountType={accountType} />
+          </div>
+        )}
+      </header>
+    </AuthProvider>
   );
 };
