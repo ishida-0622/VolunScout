be05553db300e2b8ae1b20589dc31a990289f4ec
kentaro ishida <ken.babuo@gmail.com>
2024-01-11 15:07:40 +0900

    feat: ユーザーの地域・テーマ・条件の情報を取得するエンドポイント

--- backend/query/infrastructure/src/user_account/participant.rs
+++ backend/query/infrastructure/src/user_account/participant.rs
@@ -1,10 +1,16 @@
 use anyhow::Result;
 use async_trait::async_trait;
+use domain::consts::conditions::ConditionMap;
+use domain::consts::themes::ThemeMap;
+use query_repository::MySqlBool;
 use sqlx::MySqlPool;
 
 use domain::consts::region::RegionMap;
-use domain::model::{region::Region, user_account::user_id::UserId, volunteer::Volunteer};
-use query_repository::user_account::participant::{ParticipantAccount, ParticipantUserRepository};
+use domain::model::{user_account::user_id::UserId, volunteer::Volunteer};
+use query_repository::user_account::participant::{
+    ParticipantAccount, ParticipantCondition, ParticipantRegion, ParticipantTheme,
+    ParticipantUserRepository,
+};
 
 pub struct ParticipantAccountImpl {
     pool: MySqlPool,
@@ -47,7 +53,7 @@ impl ParticipantUserRepository for ParticipantAccountImpl {
         Ok(users)
     }
 
-    async fn find_region_by_id(&self, pid: &UserId) -> Result<Vec<Region>> {
+    async fn find_region_by_id(&self, pid: &UserId) -> Result<Vec<ParticipantRegion>> {
         let response = sqlx::query!(
             r#"
             SELECT rid FROM participant_region WHERE uid = ?
@@ -59,14 +65,62 @@ impl ParticipantUserRepository for ParticipantAccountImpl {
 
         let region_map = RegionMap::new().regions_index_to_name;
 
-        let regions: Vec<Region> = response
+        let regions: Vec<ParticipantRegion> = response
             .into_iter()
-            .map(|region| {
-                Region::new(region_map.get(&(region.rid as usize)).unwrap().to_string()).unwrap()
+            .map(|region| ParticipantRegion {
+                name: region_map.get(&(region.rid as usize)).unwrap().to_string(),
             })
-            .collect::<Vec<Region>>();
+            .collect();
 
         Ok(regions)
+    }
+
+    async fn find_theme_by_id(&self, pid: &UserId) -> Result<Vec<ParticipantTheme>> {
+        let response = sqlx::query!(
+            r#"
+            SELECT * FROM participant_element WHERE uid = ?
+            "#,
+            pid.to_string()
+        )
+        .fetch_all(&self.pool)
+        .await?;
+
+        let theme_map = ThemeMap::new().themes_id_to_name;
+
+        let themes: Vec<ParticipantTheme> = response
+            .into_iter()
+            .filter(|element| theme_map.get(&element.eid).is_some())
+            .map(|element| ParticipantTheme {
+                name: theme_map.get(&element.eid).unwrap().to_string(),
+                is_required: MySqlBool::from(element.is_need),
+            })
+            .collect();
+
+        Ok(themes)
+    }
+
+    async fn find_condition_by_id(&self, pid: &UserId) -> Result<Vec<ParticipantCondition>> {
+        let response = sqlx::query!(
+            r#"
+            SELECT * FROM participant_element WHERE uid = ?
+            "#,
+            pid.to_string()
+        )
+        .fetch_all(&self.pool)
+        .await?;
+
+        let condition_map = ConditionMap::new().conditions_id_to_name;
+
+        let conditions: Vec<ParticipantCondition> = response
+            .into_iter()
+            .filter(|element| condition_map.get(&element.eid).is_some())
+            .map(|element| ParticipantCondition {
+                name: condition_map.get(&element.eid).unwrap().to_string(),
+                is_required: MySqlBool::from(element.is_need),
+            })
+            .collect();
+
+        Ok(conditions)
     }
 
     async fn find_favorite_by_id(&self, pid: &UserId) -> Result<Vec<Volunteer>> {
