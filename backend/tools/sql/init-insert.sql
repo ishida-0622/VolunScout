4a9761f0c08e1c47fa66d502121f3e059265f2fa
MasKatx <tky2102137@stu.o-hara.ac.jp>
2024-01-12 13:50:51 +0900

    fix: apply ULID into init-insert.sql

--- backend/tools/sql/init-insert.sql
+++ backend/tools/sql/init-insert.sql
@@ -52,7 +52,7 @@ INSERT INTO volunteer(
   registered_at,
   updated_at
 ) VALUES (
-  "volunteer00000000000000000",
+  "01HKXVVVKBR6G8240N7HWSPR7M",
   "group_account000000000000000",
   "Title",
   "Message",
@@ -83,7 +83,7 @@ INSERT INTO volunteer(
   registered_at,
   updated_at
 ) VALUES (
-  "volunteer11111111111111111",
+  "01HKXZRM5AF35HHXJ8284PN0B7",
   "group_account000000000000000",
   "Title",
   "Message",
@@ -99,40 +99,40 @@ INSERT INTO volunteer(
 );
 
 INSERT INTO volunteer_region VALUES (
-  "volunteer00000000000000000",
+  "01HKXVVVKBR6G8240N7HWSPR7M",
   1
 );
 
 INSERT INTO volunteer_region VALUES (
-  "volunteer00000000000000000",
+  "01HKXVVVKBR6G8240N7HWSPR7M",
   2
 );
 
 INSERT INTO volunteer_region VALUES (
-  "volunteer11111111111111111",
+  "01HKXZRM5AF35HHXJ8284PN0B7",
   3
 );
 
 INSERT INTO volunteer_region VALUES (
-  "volunteer11111111111111111",
+  "01HKXZRM5AF35HHXJ8284PN0B7",
   4
 );
 
 INSERT INTO favorite VALUES (
   "participant_account000000000",
-  "volunteer00000000000000000",
+  "01HKXVVVKBR6G8240N7HWSPR7M",
   NOW()
 );
 
 INSERT INTO favorite VALUES (
   "participant_account000000000",
-  "volunteer11111111111111111",
+  "01HKXZRM5AF35HHXJ8284PN0B7",
   NOW()
 );
 
 INSERT INTO apply VALUES (
-  "apply000000000000000000000",
-  "volunteer00000000000000000",
+  "01HKXZS5TT1GMJD8PVC5RKJH42",
+  "01HKXVVVKBR6G8240N7HWSPR7M",
   "participant_account000000000",
   "2024-1-12 12:00:00",
   true,
@@ -142,8 +142,8 @@ INSERT INTO apply VALUES (
 );
 
 INSERT INTO apply VALUES (
-  "apply000000000000000000001",
-  "volunteer00000000000000000",
+  "01HKXZSD39EDT7F528X87EPHX9",
+  "01HKXVVVKBR6G8240N7HWSPR7M",
   "participant_account000000001",
   "2024-1-13 12:00:00",
   false,
@@ -153,7 +153,7 @@ INSERT INTO apply VALUES (
 );
 
 INSERT INTO group_participants VALUES (
-  "apply000000000000000000000",
+  "01HKXZS5TT1GMJD8PVC5RKJH42",
   1,
   "first taro",
   "first furigana",
@@ -162,7 +162,7 @@ INSERT INTO group_participants VALUES (
 );
 
 INSERT INTO group_participants VALUES (
-  "apply000000000000000000000",
+  "01HKXZS5TT1GMJD8PVC5RKJH42",
   2,
   "second hanako",
   "second furigana",
