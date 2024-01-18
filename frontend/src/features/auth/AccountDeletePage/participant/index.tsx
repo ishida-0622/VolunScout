40c55ef3499f61eb27bf63b2764b0ad1934a6422
Na4an00724 <fumiya0108@ozzio.jp>
2024-01-18 11:44:49 +0900

    feat: グループ・参加者の退会画面のCSS

--- frontend/src/features/auth/AccountDeletePage/participant/index.tsx
+++ frontend/src/features/auth/AccountDeletePage/participant/index.tsx
@@ -7,6 +7,7 @@ import styles from "./index.module.css";
 import type { DeleteParticipantAccountRequestBody } from "@/__generated__/command";
 
 import { apiClientParticipant } from "@/api/command";
+import { joinClassnames } from "@/components/@joinClassnames";
 import { BackButton } from "@/components/ui-parts/BackButton";
 import { useAuthContext } from "@/contexts/AuthContext";
 import { useLogout } from "@/features/auth/hooks/useLogout";
@@ -35,24 +36,31 @@ export const AccountDeletePage = () => {
 
   return (
     <div className={styles.base}>
-      <BackButton />
-      <h1>退会（アカウントの削除）に関する確認</h1>
-      <p>
-        アカウントを削除すると、これまでに登録した以下のような情報が全て削除され、
-        アカウントの復元をすることができなくなります。
-      </p>
-      <ul>
-        <li>個人情報・登録情報</li>
-        <li>ボランティア活動・応募の履歴</li>
-        <li>お気に入りに登録したボランティアの履歴</li>
-        <li>その他のアカウントに紐づいているデータ</li>
-      </ul>
-      <p>
-        尚、現在参加予定のボランティアがある場合、退会することができません。
-        また、この操作は取り消すことができません。
-      </p>
-      <p>退会してもよろしいですか？</p>
-      <SubmitButton onClick={handleOnClick} />
+      <BackButton className={joinClassnames("btn btn-primary")} />
+      <h1 className={styles.top}>退会（アカウントの削除）に関する確認</h1>
+      <div className={styles.main_contents}>
+        <p>
+          アカウントを削除すると、これまでに登録した以下のような情報が全て削除され、
+          アカウントの復元をすることができなくなります。
+        </p>
+        <ul>
+          <li>参加者情報・登録情報</li>
+          <li>登録したボランティア活動</li>
+          <li>参加者の履歴</li>
+          <li>その他のアカウントに紐づいているデータ</li>
+        </ul>
+        <p>
+          尚、現在活動予定のボランティアがある場合、退会することができません。
+          また、この操作は取り消すことができません。
+        </p>
+        <p>退会してもよろしいですか？</p>
+      </div>
+      <div className={styles.button}>
+        <SubmitButton
+          onClick={handleOnClick}
+          className={joinClassnames("btn btn-danger")}
+        />
+      </div>
     </div>
   );
 };
