e1f30231e21704024498e199fbf63c511320ab84
kentaro ishida <ken.babuo@gmail.com>
2024-02-03 00:41:26 +0900

    feat: api client の追加

--- frontend/src/api/command/apiClient.ts
+++ frontend/src/api/command/apiClient.ts
@@ -1,7 +1,9 @@
 import {
   Configuration,
+  ControllersApplyApi,
   ControllersGroupApi,
   ControllersParticipantApi,
+  ControllersReviewApi,
   ControllersVolunteerApi,
 } from "@/__generated__/command";
 
@@ -18,6 +20,18 @@ export const apiClientParticipant = new ControllersParticipantApi(
 );
 
 export const apiClientVolunteer = new ControllersVolunteerApi(
+  new Configuration({
+    basePath: process.env.NEXT_PUBLIC_WRITE_API_SERVER_BASE_URL,
+  })
+);
+
+export const apiClientApply = new ControllersApplyApi(
+  new Configuration({
+    basePath: process.env.NEXT_PUBLIC_WRITE_API_SERVER_BASE_URL,
+  })
+);
+
+export const apiClientReview = new ControllersReviewApi(
   new Configuration({
     basePath: process.env.NEXT_PUBLIC_WRITE_API_SERVER_BASE_URL,
   })
