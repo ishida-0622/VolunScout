d63b371d3816365aeee8acc752fe6b5bc59c035e
kentaro ishida <ken.babuo@gmail.com>
2024-02-07 14:59:54 +0900

    feat: Google Calender に予定を追加するボタンを追加

--- frontend/src/features/volunteer/participant/VolunteerItem/index.tsx
+++ frontend/src/features/volunteer/participant/VolunteerItem/index.tsx
@@ -1,26 +1,46 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import { Col, Row } from "react-bootstrap";
+import { Button, Col, Row } from "react-bootstrap";
 
 import { FavButton } from "../FavButton";
 
-import type { GetVolunteerByGidQuery } from "@/__generated__/query/graphql";
+import type { GetFavQuery } from "@/__generated__/query/graphql";
 
 import { URL_PATH_PARTICIPANT } from "@/consts";
+import { dateTimeToGoogleCalendarFormat } from "@/utils/dateTimeToGoogleCalendarFormat";
 import { formatDate } from "@/utils/formatDate";
 
 type Props = {
-  volunteer: GetVolunteerByGidQuery["volunteers"][number];
+  volunteer: GetFavQuery["volunteers"][number];
   initIsFav: boolean;
   fav?: () => void;
   unFav?: () => void;
+  isCalendar?: boolean;
 };
 
-export const VolunteerItem = ({ volunteer, initIsFav, fav, unFav }: Props) => {
+export const VolunteerItem = ({
+  volunteer,
+  initIsFav,
+  fav,
+  unFav,
+  isCalendar,
+}: Props) => {
   const router = useRouter();
   const toVolunteer = () => {
     router.push(URL_PATH_PARTICIPANT.VOLUNTEER_DETAIL(volunteer.vid));
+  };
+
+  const addGoogleCalendar = () => {
+    // 別タブでGoogleカレンダーに追加する
+    const url = `https://www.google.com/calendar/render?action=TEMPLATE&text=${
+      volunteer.title
+    }&details=${volunteer.overview}&location=${
+      volunteer.place
+    }&dates=${dateTimeToGoogleCalendarFormat(
+      volunteer.startAt
+    )}/${dateTimeToGoogleCalendarFormat(volunteer.finishAt)}`;
+    window.open(url, "_blank");
   };
 
   return (
@@ -29,7 +49,7 @@ export const VolunteerItem = ({ volunteer, initIsFav, fav, unFav }: Props) => {
       style={{ maxWidth: "1080px" }}
     >
       <Row className="align-items-center">
-        <Col sm={11} onClick={toVolunteer} role="button">
+        <Col sm={10} onClick={toVolunteer} role="button">
           <Row>
             <Col>
               <h2>{volunteer.title}</h2>
@@ -40,11 +60,13 @@ export const VolunteerItem = ({ volunteer, initIsFav, fav, unFav }: Props) => {
               <span>場所：</span>
               <span>{volunteer.place}</span>
             </Col>
-            <Col sm="3">
+            <Col sm="5">
               <span>日時：</span>
               <span>{formatDate(volunteer.startAt)}</span>
+              <span>～</span>
+              <span>{formatDate(volunteer.finishAt)}</span>
             </Col>
-            <Col sm="3">
+            <Col sm="1">
               <span>人数：</span>
               <span>{volunteer.recruitedNum}</span>
             </Col>
@@ -53,13 +75,30 @@ export const VolunteerItem = ({ volunteer, initIsFav, fav, unFav }: Props) => {
             <Col>{volunteer.overview}</Col>
           </Row>
         </Col>
-        <Col sm={1}>
-          <FavButton
-            vid={volunteer.vid}
-            initIsFav={initIsFav}
-            onFavClick={fav}
-            onUnFavClick={unFav}
-          />
+        <Col sm={2}>
+          <Row>
+            <Col className="text-center">
+              <FavButton
+                vid={volunteer.vid}
+                initIsFav={initIsFav}
+                onFavClick={fav}
+                onUnFavClick={unFav}
+              />
+            </Col>
+          </Row>
+          <Row>
+            {isCalendar && (
+              <Col>
+                <Button size="sm" className="w-100" onClick={addGoogleCalendar}>
+                  <span>
+                    Googleカレンダー
+                    <br />
+                    に追加
+                  </span>
+                </Button>
+              </Col>
+            )}
+          </Row>
         </Col>
       </Row>
     </div>
