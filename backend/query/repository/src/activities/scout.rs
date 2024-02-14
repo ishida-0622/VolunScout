c3281392b730ad5732c58decd50b14acadf0a4e8
kentaro ishida <ken.babuo@gmail.com>
2024-02-15 08:46:49 +0900

    feat: クエリを変更

--- backend/query/repository/src/activities/scout.rs
+++ backend/query/repository/src/activities/scout.rs
@@ -1,11 +1,40 @@
 use anyhow::Result;
 use async_graphql::SimpleObject;
 use async_trait::async_trait;
-use chrono::NaiveDateTime;
+use chrono::{NaiveDate, NaiveDateTime};
 
-use domain::model::{user_account::user_id::UserId, scout::ScoutId, volunteer::VolunteerId};
+use domain::model::{scout::ScoutId, user_account::user_id::UserId, volunteer::VolunteerId};
 
 /// スカウトリードモデル
+#[derive(SimpleObject, sqlx::Type)]
+pub struct ScoutFromGroup {
+    // スカウトID
+    pub sid: String,
+    // ボランティアID
+    pub vid: String,
+    // 参加者ID
+    pub uid: String,
+    // スカウトメッセージ
+    pub message: String,
+    // スカウト日時
+    pub scouted_at: NaiveDateTime,
+    // 既読有無
+    pub is_read: bool,
+    // スカウトメール送信有無
+    pub is_sent: bool,
+    // スカウトメール送信日時
+    pub sent_at: Option<NaiveDateTime>,
+    // 辞退有無
+    pub is_denied: bool,
+    // 辞退日時
+    pub denied_at: Option<NaiveDateTime>,
+
+    pub name: String,
+    pub gender: u8,
+    pub birthday: NaiveDate,
+    pub point: f32,
+}
+
 #[derive(SimpleObject, sqlx::Type)]
 pub struct Scout {
     // スカウトID
@@ -27,7 +56,7 @@ pub struct Scout {
     // 辞退有無
     pub is_denied: bool,
     // 辞退日時
-    pub denied_at: Option<NaiveDateTime>
+    pub denied_at: Option<NaiveDateTime>,
 }
 
 impl Scout {
@@ -41,7 +70,7 @@ impl Scout {
         is_sent: bool,
         sent_at: Option<NaiveDateTime>,
         is_denied: bool,
-        denied_at: Option<NaiveDateTime>
+        denied_at: Option<NaiveDateTime>,
     ) -> Scout {
         Scout {
             sid,
@@ -53,7 +82,7 @@ impl Scout {
             is_sent,
             sent_at,
             is_denied,
-            denied_at
+            denied_at,
         }
     }
 }
@@ -70,5 +99,5 @@ pub trait ScoutRepository: Send + Sync {
     async fn find_by_uid(&self, uid: &UserId) -> Result<Vec<Scout>>;
 
     /// スカウト情報をボランティアIDで一括取得する
-    async fn find_by_vid(&self, vid: &VolunteerId) -> Result<Vec<Scout>>;
+    async fn find_by_vid(&self, vid: &VolunteerId) -> Result<Vec<ScoutFromGroup>>;
 }
